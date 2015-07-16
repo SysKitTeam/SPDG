f8b8d921f1ffed46c9a59d798e59fd24090d2993
kkorovljevic <kkorovljevic@gmail.com>
2015-07-16 16:19:20 +0200

    Lists and Folders

---
+++ Acceleratio.SPDG.Generator/03Lists.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace Acceleratio.SPDG.Generator
+{
+    public partial class DataGenerator
+    {
+        SPListTemplateType lastTemplateType = SPListTemplateType.NoListTemplate;
+        string lastListPrefix = "List";
+
+        public void CreateLists()
+        {
+            foreach (SiteCollInfo siteCollInfo in workingSiteCollections)
+            {
+                using (SPSite siteColl = new SPSite(siteCollInfo.URL))
+                {
+                    foreach (SiteInfo siteInfo in siteCollInfo.Sites)
+                    {
+                        using( SPWeb web = siteColl.OpenWeb(siteInfo.URL)) 
+                        {
+                            for( int s = 0; s < workingDefinition.MaxNumberOfListsAndLibrariesPerSite; s++ )
+                            {
+                                string listNum = (s + 1).ToString("00");
+                                getNextTemplateType();
+                                web.Lists.Add(lastListPrefix + listNum, string.Empty, lastTemplateType);
+
+                                ListInfo listInfo = new ListInfo();
+                                listInfo.Name = lastListPrefix + listNum;
+
+                                siteInfo.Lists.Add(listInfo);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+        private SPListTemplateType getNextTemplateType()
+        {
+            bool changed = false;
+            if (workingDefinition.LibTypeList && lastTemplateType != SPListTemplateType.GenericList)
+            {
+                lastTemplateType = SPListTemplateType.GenericList;
+                lastListPrefix = "List";
+                changed = true;
+            }
+
+            if( !changed && workingDefinition.LibTypeDocument && lastTemplateType != SPListTemplateType.DocumentLibrary)
+            {
+                lastTemplateType = SPListTemplateType.DocumentLibrary;
+                lastListPrefix = "Library";
+                changed = true;
+            }
+
+            if (!changed && workingDefinition.LibTypeTasks && lastTemplateType != SPListTemplateType.Tasks)
+            {
+                lastTemplateType = SPListTemplateType.DocumentLibrary;
+                lastListPrefix = "Tasks";
+                changed = true;
+            }
+
+            return lastTemplateType;
+        }
+    }
+}
---
+++ Acceleratio.SPDG.Generator/04Folders.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace Acceleratio.SPDG.Generator
+{
+    public partial class DataGenerator
+    {
+        public void CreateFolders()
+        {
+            foreach (SiteCollInfo siteCollInfo in workingSiteCollections)
+            {
+                using (SPSite siteColl = new SPSite(siteCollInfo.URL))
+                {
+                    foreach (SiteInfo siteInfo in siteCollInfo.Sites)
+                    {
+                        using (SPWeb web = siteColl.OpenWeb(siteInfo.URL))
+                        {
+                            foreach (ListInfo listInfo in siteInfo.Lists)
+                            {
+                                if(listInfo.Name.StartsWith("Library"))
+                                {
+                                    for (int counter = 1; counter <= workingDefinition.MaxNumberOfFoldersToGenerate; counter++)
+                                    {
+                                        string folderNumber = (counter).ToString("00");
+                                        SPList list = web.Lists[listInfo.Name];
+                                        SPFolder folder = list.RootFolder.SubFolders.Add("Folder" + folderNumber);
+                                        folder.Update();
+
+                                        FolderInfo folderInfo = new FolderInfo();
+                                        folderInfo.Name = "Folder" + folderNumber;
+                                        listInfo.Folders.Add(folderInfo);
+
+                                        for (int l = 1; l < workingDefinition.MaxNumberOfNestedFolderLevelPerLibrary; l++)
+                                        {
+                                            counter++;
+                                            if (counter >= workingDefinition.MaxNumberOfFoldersToGenerate)
+                                            {
+                                                break;
+                                            }
+
+                                            folderNumber = (counter).ToString("00");
+                                            folder = folder.SubFolders.Add("Folder" + folderNumber);
+                                            //folder.Name = "Folder" + folderNumber;
+                                            folder.Update();
+
+                                            FolderInfo folderInfo2 = new FolderInfo();
+                                            folderInfo2.Name = "Folder" + folderNumber;
+                                            listInfo.Folders.Add(folderInfo2);
+                                        }
+                                    }
+
+                                     
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
